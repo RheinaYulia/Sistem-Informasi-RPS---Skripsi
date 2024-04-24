528d3cc477ba8b78fa2b0a571a17104528dec338
RheinaYulia <rheinayulia27@gmail.com>
2024-04-24 19:03:46 +0700

    User

--- database/seeders/SUserSeeder.php
+++ database/seeders/SUserSeeder.php
@@ -18,33 +18,40 @@ class SUserSeeder extends Seeder
             [
                 'user_id'   => 1, // Super Admin
                 'group_id'  => 1, // Super Admin
-                'username'  => 'superadmin',
-                'name'      => 'Super Administrator',
-                'email'     => 'super@admin.com',
-                'password'  => password_hash('12345', PASSWORD_DEFAULT),
-            ],[
-                'user_id'   => 2, // Admin
-                'group_id'  => 2, // Admin
                 'username'  => 'admin',
                 'name'      => 'Administrator',
                 'email'     => 'admin@admin.com',
                 'password'  => password_hash('12345', PASSWORD_DEFAULT),
             ],[
+                'user_id'   => 2, // Admin
+                'group_id'  => 2, // Admin
+                'username'  => 'teamtc',
+                'name'      => 'Team Teaching',
+                'email'     => 'ttc@admin.com',
+                'password'  => password_hash('12345', PASSWORD_DEFAULT),
+            ],[
                 'user_id'   => 3, //
                 'group_id'  => 3, // Dosen
-                'username'  => 'dosen',
-                'name'      => 'Dosen',
-                'email'     => 'dosen@admin.com',
+                'username'  => 'dosenkurikulum',
+                'name'      => 'Dosen Kurikulum',
+                'email'     => 'dosenkurikulum@admin.com',
                 'password'  => password_hash('12345', PASSWORD_DEFAULT),
             ],
             [
                 'user_id'   => 4, //
                 'group_id'  => 4, // Mahasiswa
-                'username'  => 'mahasiswa',
-                'name'      => 'Mahasiswa',
-                'email'     => 'mahasiswa@admin.com',
+                'username'  => 'kaprodi',
+                'name'      => 'Kaprodi',
+                'email'     => 'kaprodi@admin.com',
                 'password'  => password_hash('12345', PASSWORD_DEFAULT),
-            ]
+            ],[
+                'user_id'   => 5, //
+                'group_id'  => 4, // Dosen
+                'username'  => 'dosen',
+                'name'      => 'Dosen',
+                'email'     => 'dosen@admin.com',
+                'password'  => password_hash('12345', PASSWORD_DEFAULT),
+            ],
         ]);
     }
 }
